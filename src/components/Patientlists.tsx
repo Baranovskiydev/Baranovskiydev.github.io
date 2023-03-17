cffc9165369b8de823a0cff4368023d04750f6ee
Ilya Baranovskiy <baranovskiydev@gmail.com>
2023-03-17 09:00:07 +0300

    cummit

---
+++ src/components/Patientlists.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./components.module.css"
+
+function Patientlists() {
+  return (
+    <div className={styles.list}>
+        
+    </div>
+  )
+}
+
+export default Patientlists
